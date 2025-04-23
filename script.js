71cb5c74fd150a652e44a9f63eefbaf5cc66239c
vitalya <vitalay.efimov@gmail.com>
2025-04-23 22:54:17 +0500

    Task 1 Кнопка b-1 запускает функцию t1. Функция должна выводить в out-1 строку вида:
    1 2 3 4 5 6 7 8 9 ... 49 50
    
    от 1 до 50 включительно. Разделитель - пробел. Задача решается с помощью цикла.

--- script.js
+++ script.js
@@ -15,20 +15,26 @@
 //   b[i].style.border = '5px solid green';
 //   console.log(b);
 // }
-document.querySelector('button').onclick = () => {
-  let r = document.querySelectorAll('input[type="radio"]');
-console.log(r);
- for (let i = 0; i < r.length; i++) {
-   if (r[i].checked) {
-     console.log(r[i].value);
-   }
- }
+// document.querySelector('button').onclick = () => {
+//   let r = document.querySelectorAll('input[type="radio"]');
+// console.log(r);
+//  for (let i = 0; i < r.length; i++) {
+//    if (r[i].checked) {
+//      console.log(r[i].value);
+//    }
+//  }
      
+// }
+// let two = '';
+// for (let i =0; i < 10; i++) {
+//   if (i == 6) continue;
+//   two += i + ' ';
+// }
+// document.querySelector('.two').innerHTML = two;
+const out = document.querySelector('.out-1')
+const btn = document.querySelector('.b-1').addEventListener('click', function t1(){
+  for (let i = 1; i <= 50; i++){
+   out.innerHTML += i + ' ';
 }
-let two = '';
-for (let i =0; i < 10; i++) {
-  if (i == 6) continue;
-  two += i + ' ';
-}
-document.querySelector('.two').innerHTML = two;
+})
 
